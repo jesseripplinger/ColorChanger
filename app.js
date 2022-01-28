19769f74a3a440f7b7490ab533bd9ed53cbfa1b1
jesseripplinger <jesse.r109@slcstudents.org>
2022-01-28 15:07:22 -0700

    updated version

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["black", "white", "black", "white"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click",function () {
-    const randomNumber = getRadomNumber();
-    console.log(randomNumber)
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+   color.textContent = colors[randomNumber];
 });
 
-function getRandomNumbers() {
-    return Math.floor(Math.random() * colors.length;);
-}
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}z
